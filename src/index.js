2b7587a2cda558ff88237709e8127b0a2b3ce00a
jxldemo <1842525012@qq.com>
2019-11-01 14:48:17 +0800

    已修改

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+// import './index.css'
 
+// const name='1705B'
+// const element=<p>{name+'班级'}</p>
 
+// function fromatName(){
+// 	return '小白'+' '+'小黑'
+// }
+// const html=(
+// 	<h1>
+// 		游戏学院{'1705B'+fromatName()}
+// 	</h1>
+// )
+
+const arrt={
+	name:1,
+	age:18
+}
+const html=(
+	<div>
+		<span dataType='a0'>a0</span>
+		<span data={arrt.name}>{arrt.name}</span>
+		<span classname='cls'>cla</span>
+		<span classanme={arrt.age}>{arrt.age}</span>
+	</div>
+)
 ReactDOM.render(
-	<p>宫本一海</p>,
+	// element,
+	html,
 	document.getElementById('root')
 )
