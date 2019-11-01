ad2c6ba6b300ab1f2054179df0cd4f3696f638b5
dogegger <1395794660@qq.com>
2019-11-01 15:52:05 +0800

    我好气

--- src/index.js
+++ src/index.js
@@ -2,12 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
+// const html = React.createElement(
+//   'h1',
+//   { className: 'greeting' },
+//   'Hello world',
+// )
 
-function tick() {
-  ReactDOM.render(
-    <Clock date={new Date()} />,
-    document.getElementById('root')
-  )
+const num = 5
+let element = ''
+
+if(num > 1) {
+  element = <h1>1705B</h1>
+} else {
+  element = <h1>1706B</h1>
 }
 
-setInterval(tick, 1000)
+element = <div>{element}</div>
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+)
