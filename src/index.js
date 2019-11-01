18b58b9e56f4b590a107b78bd62dd96ce59530bc
ch <2564510610@qq.com>
2019-11-01 14:47:24 +0800

    two

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
+const html = React.createElement(
+  'h1',
+  {className: 'A',value: 1},
+  'end'
+)
+
 // ReactDOM.render 渲染
 ReactDOM.render(
-  // <p>Hello World</p>,
-  <p>33333333</p>,
+  html,
   document.getElementById('root')
 )
