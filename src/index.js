5fb3d1ba9d93b8937a628a2a1719260f8b7824ae
王亮 <1937444194@qq.com>
2019-11-01 10:50:21 +0800

    第一课

--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-
-function tick() {
-  ReactDOM.render(
-    <Clock date={new Date()} />,
-    document.getElementById('root')
-  )
-}
-
-setInterval(tick, 1000)
+// ReactDOM.render 渲染
+ReactDOM.render(
+  // <p>Hello World</p>,
+  <p>33333333</p>,
+  document.getElementById('root')
+)
