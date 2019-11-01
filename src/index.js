555d58644c63af4cdc666cbfe50b443e612e7c87
mazhenchuan <1600516109@qq.com>
2019-11-01 13:38:46 +0800

    '第一课'

--- src/index.js
+++ src/index.js
@@ -3,11 +3,42 @@ import ReactDOM from 'react-dom'
 import './index.css'
 
 
-function tick() {
-  ReactDOM.render(
-    <Clock date={new Date()} />,
-    document.getElementById('root')
-  )
-}
+// function tick() {
+//   ReactDOM.render(
+//     <Clock date={new Date()} />,
+//     document.getElementById('root')
+//   )
+// }
 
-setInterval(tick, 1000)
+// setInterval(tick, 1000)
+
+const name = "1705B"
+const element = <div>{name+'班'}</div>
+const count = '20人'
+const num = <div>{element}{count}</div>
+ReactDOM.render(
+  num,
+  document.getElementById('root')
+)
+const from = {
+  name:"小明",
+  age:"20"
+}
+function dag(){
+  return from.name + '' + from.age
+}
+const htmls = (
+  <div className = {from.name}>
+    {dag()}
+  </div>
+)
+ReactDOM.render(
+  htmls,
+  document.getElementById('root1')
+)
+
+let countnum = false?'已售':'未受'
+ReactDOM.render(
+  <div>{countnum}</div>,
+  document.getElementById('root2')
+)
