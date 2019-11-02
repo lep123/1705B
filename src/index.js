3726e543c5e8777a227f0eb0817ab82a7e462665
ch <2564510610@qq.com>
2019-11-02 17:04:47 +0800

    11.2作业

--- src/index.js
+++ src/index.js
@@ -1,15 +1,141 @@
-import React from 'react'
+import React, {
+	Component
+} from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-const html = React.createElement(
-  'h1',
-  {className: 'A',value: 1},
-  'end'
-)
+function textStrong(res) {
+	return <b>{res}</b>
+}
 
-// ReactDOM.render 渲染
+const goods = [{
+	id: 1,
+	child: [{
+		url: "image/1.jpg",
+		name: "呜呜呜呜呜呜呜呜无无无无无无无无无无无无无无无无无无无无无无无无"
+	}, {
+		url: "image/1.jpg",
+		name: "呜呜呜呜呜呜呜呜无无无无无无无无无无无无无无无无无无无无无无无无"
+	}, {
+		url: "image/1.jpg",
+		name: "呜呜呜呜呜呜呜呜无无无无无无无无无无无无无无无无无无无无无无无无"
+	}, {
+		url: "image/1.jpg",
+		name: "呜呜呜呜呜呜呜呜无无无无无无无无无无无无无无无无无无无无无无无无"
+	}, {
+		url: "image/1.jpg",
+		name: "呜呜呜呜呜呜呜呜无无无无无无无无无无无无无无无无无无无无无无无无"
+	}, {
+		url: "image/1.jpg",
+		name: "呜呜呜呜呜呜呜呜无无无无无无无无无无无无无无无无无无无无无无无无"
+	}]
+}, {
+	id: 2,
+	child: [{
+		url: "image/2.jpg",
+		name: "呜呜呜呜呜呜呜呜无无无无无无无无无无无无无无无无无无无无无无无无"
+	}, {
+		url: "image/2.jpg",
+		name: "呜呜呜呜呜呜呜呜无无无无无无无无无无无无无无无无无无无无无无无无"
+	}, {
+		url: "image/2.jpg",
+		name: "呜呜呜呜呜呜呜呜无无无无无无无无无无无无无无无无无无无无无无无无"
+	}, {
+		url: "image/2.jpg",
+		name: "呜呜呜呜呜呜呜呜无无无无无无无无无无无无无无无无无无无无无无无无"
+	}, {
+		url: "image/2.jpg",
+		name: "呜呜呜呜呜呜呜呜无无无无无无无无无无无无无无无无无无无无无无无无"
+	}, {
+		url: "image/2.jpg",
+		name: "呜呜呜呜呜呜呜呜无无无无无无无无无无无无无无无无无无无无无无无无"
+	}]
+}, {
+	id: 3,
+	child: [{
+		url: "image/3.jpg",
+		name: "呜呜呜呜呜呜呜呜无无无无无无无无无无无无无无无无无无无无无无无无"
+	}, {
+		url: "image/3.jpg",
+		name: "呜呜呜呜呜呜呜呜无无无无无无无无无无无无无无无无无无无无无无无无"
+	}, {
+		url: "image/3.jpg",
+		name: "呜呜呜呜呜呜呜呜无无无无无无无无无无无无无无无无无无无无无无无无"
+	}, {
+		url: "image/3.jpg",
+		name: "呜呜呜呜呜呜呜呜无无无无无无无无无无无无无无无无无无无无无无无无"
+	}, {
+		url: "image/3.jpg",
+		name: "呜呜呜呜呜呜呜呜无无无无无无无无无无无无无无无无无无无无无无无无"
+	}, {
+		url: "image/3.jpg",
+		name: "呜呜呜呜呜呜呜呜无无无无无无无无无无无无无无无无无无无无无无无无"
+	}]
+}]
+
+const titleName = [{
+	id: 1,
+	name: "水果",
+	bol: true
+}, {
+	id: 2,
+	name: "女装",
+	bol: false
+}, {
+	id: 3,
+	name: "男装",
+	bol: false
+}, ]
+
+function fn(id) {
+	console.log(id)
+	ReactDOM.render(
+		<div>
+			<Shoping />
+			<Goods search={id}/>
+		</div>,
+		document.getElementById("root")
+	)
+}
+
+class Goods extends Component {
+	render() {
+		console.log()
+		/*let content = goods.filter(v => this.props.search == v.id)
+		console.log(content[0])
+		content[0].child.map(v => <h3>{v.name}</h3>)*/
+		return (
+			<div className="goods">
+				{
+					goods.filter(v => this.props.search == v.id)[0]
+					.child.map(v => <h3>
+						<img src={v.url} />
+						<span>{v.name}</span>
+						<p><b>促销价</b><i>￥1？5</i></p>
+					</h3>)
+				}
+			</div>
+		)
+	}
+}
+
+class Shoping extends Component {
+	render() {
+		return (
+			<p className="title">
+				{titleName.map(v => <span
+					onClick={() => {
+						fn(v.id)
+					}}
+					id={v.id}>{v.name}</span>)}
+			</p>
+		)
+	}
+}
 ReactDOM.render(
-  html,
-  document.getElementById('root')
+	<div>
+		<Shoping />
+		<Goods search="1"/>
+	</div>,
+	document.getElementById("root")
 )
