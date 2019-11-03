8faf5f893a25bc344edcf13fc2e368e6149f283b
mazhenchuan <1600516109@qq.com>
2019-11-03 19:00:47 +0800

    123

--- src/index.js
+++ src/index.js
@@ -2,84 +2,133 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
+let arr = ['水果','男装','女装']
+let sectionInfo =[
+                  {
+                    id:0,
+                    children:[
+                              {img:"images/a1_03.gif",name:"水果水果111111111111111111",price:"￥175"},
+                              {img:"images/a1_03.gif",name:"水果水果111111111111111111",price:"￥175"},
+                              {img:"images/a1_03.gif",name:"水果水果111111111111111111",price:"￥175"},
+                              {img:"images/a1_03.gif",name:"水果水果111111111111111111",price:"￥175"},
+                              {img:"images/a1_03.gif",name:"水果水果111111111111111111",price:"￥175"},
+                              {img:"images/a1_03.gif",name:"水果水果111111111111111111",price:"￥175"}
+                            ]
+                  },
+                  {
+                    id:1,
+                    children:[
+                      {img:"images/a1_03.gif",name:"水果水果111111111111111111",price:"￥275"},
+                      {img:"images/a1_03.gif",name:"水果水果111111111111111111",price:"￥275"},
+                      {img:"images/a1_03.gif",name:"水果水果111111111111111111",price:"￥275"},
+                      {img:"images/a1_03.gif",name:"水果水果111111111111111111",price:"￥275"},
+                      {img:"images/a1_03.gif",name:"水果水果111111111111111111",price:"￥275"},
+                      {img:"images/a1_03.gif",name:"水果水果111111111111111111",price:"￥275"}
+                    ]
+                  },
+                  {
+                    id:2,
+                    children:[
+                      {img:"images/a1_03.gif",name:"水果水果111111111111111111",price:"￥375"},
+                      {img:"images/a1_03.gif",name:"水果水果111111111111111111",price:"￥375"},
+                      {img:"images/a1_03.gif",name:"水果水果111111111111111111",price:"￥375"},
+                      {img:"images/a1_03.gif",name:"水果水果111111111111111111",price:"￥375"},
+                      {img:"images/a1_03.gif",name:"水果水果111111111111111111",price:"￥375"},
+                      {img:"images/a1_03.gif",name:"水果水果111111111111111111",price:"￥375"}
+                    ]
+                  }
+]
 
-// function tick() {
-//   ReactDOM.render(
-//     <Clock date={new Date()} />,
-//     document.getElementById('root')
+
+function Info() {
+  var count
+  return (
+    <div>
+      
+      <ul>
+        {
+          arr.map(function(ele,index,arr){
+              /*Each child in an array or iterator should have a unique "key" prop.*/
+              
+              return <li key={index} className="lis" onClick={function() {
+                count = {index}.index
+                console.log(count)
+              }}>{ele}</li>
+          })
+        }
+      </ul>
+      {/* <div>
+        
+        {
+          console.log(count),
+          sectionInfo[0].children.map(function(ele,index,arr){
+            console.log(ele.img)
+            return (
+              <dl key={index}>
+                <dt>
+                  
+                  <img src={ele.img} />
+                </dt>
+                <dd>
+                  <p>{ele.name}</p>
+                  <p>{ele.price}</p>
+                </dd>
+              </dl>
+            )
+          })
+        }
+      </div> */}
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <Info/>
+  ,document.getElementById('root')
+)
+
+// function Imgs(porps) {
+//   return (
+//     <div>
+//     <img src={porps.porps} />
+//     </div>
+//   ) 
+  
+// }
+
+
+// function User(propos) {
+//   return (
+//     <div>
+//       <Imgs porps={propos.propos.avatarUrl}/>
+//     <p>{propos.propos.name}</p>
+//     </div>
 //   )
 // }
 
-// setInterval(tick, 1000)
+// function Comment(propos) {
+//   console.log(propos)
+//   return (
+//     <div>
+//       <User propos={propos.author} />
 
-const name = "1705B"
-const element = <div>{name+'班'}</div>
-const count = '20人'
-const num = <div>{element}{count}</div>
-ReactDOM.render(
-  num,
-  document.getElementById('root')
-)
-const from = {
-  name:"小明",
-  age:"20",
-  img:"logo192.png"
-}
-function dag(){
-  return from.name + '' + from.age
-}
-function tick(){
-  dag()
-}
-const htmls = (
-  <div className = {from.name}>
-    {dag()}
-    <p>
-    {new Date()}
-    </p>
-  </div>
-)
-setTimeout(tick,1000)
-ReactDOM.render(
-  htmls,
-  document.getElementById('root1')
-)
+//       <li>{propos.text}</li>
+//       <li>{propos.date}</li>
+//     </div>
+//   )
+// }
+
+// ReactDOM.render(
+//   <Comment
+//     author = {{
+//       avatarUrl:'https://www.baidu.com/img/bd_logo1.png',
+//       name:'xsa',
+//     }}
+//     text = "内容啊实打实风微风维吾尔个人个人"
+//     date = {String(new Date())}
+//   />,document.getElementById('root')
+// )
 
 
 
-let maStyle = {
-  fontSize: 20,
-  color: "red",
-  marginTop:20
-}
-let numbers = 1;
-let countnum = false?'已售':'未受'
-ReactDOM.render(
-  <div style={maStyle}>
-    {countnum}
-    <button onClick= {
-      //点击事件
-      function fn () {
-        console.log(1)
-      }
-    }>click me</button>
-    <img src={from.img} />
-    
-    <p>{ numbers == 2 ?"true":"fasle"}</p>
-  </div>,
-  document.getElementById('root2')
-)
-
-
-var names = ['Alice', 'Emily', 'Kate']; 
-ReactDOM.render(
-    <div>
-        {
-            names.map(function(ele,index,arr){
-                /*Each child in an array or iterator should have a unique "key" prop.*/
-                return <h1>{ele}</h1>
-            })
-        }
-    </div>, document.getElementById('example') 
-)
 
