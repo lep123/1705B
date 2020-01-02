0f5a6d9f90fdf09a87d95c2f4ba1485e426ada3a
wxy <535025152@qq.com>
2020-01-02 20:00:13 +0800

    'and'

--- app/src/index.js
+++ app/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Route from '@/router'
 import './index.less';
+import 'antd/dist/antd.css';
 
 
 ReactDOM.render(
--- app/src/pages/login/index.jsx
+++ app/src/pages/login/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import './styles.less'
 
 export default class extends React.Component {
+
 	render() {
 		return (
 			<div>
-				Login
+
 			</div>
 		)
 	}
