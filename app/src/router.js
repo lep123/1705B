b43ed071032c3a6293a3348fefcc03c50311e7de
wxy <535025152@qq.com>
2020-01-02 21:03:58 +0800

    'login'

--- app/src/router.js
+++ app/src/router.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import Login from '@/pages/login'
-import Home from '@/pages/home'
 import { Provider } from 'react-redux'
 import { persistStore } from 'redux-persist'
 import { PersistGate } from 'redux-persist/lib/integration/react'
@@ -10,6 +8,7 @@ import loadable  from '@/utils/loader'
 
 const Home = loadable(() => import('./pages/home'))
 const Login = loadable(() => import('./pages/login'))
+const Registered = loadable(() => import('./pages/registered'))
 
 export default function Router() {
 	return (
@@ -17,6 +16,7 @@ export default function Router() {
 			<PersistGate loading={null} persistor={persistStore(store)}>
 				<BrowserRouter>
 					<Switch>
+						<Route path="/registered" component={Registered}/>
 						<Route path="/login" component={Login} />
 						<Route path="/" component={Home} />
 					</Switch>
