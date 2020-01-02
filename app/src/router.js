bf1dc837b2dfdc234da1f6c9a6d3b376e7c332e1
kb <2264805401@qq.com>
2020-01-02 20:22:29 +0800

    '王潇瑜傻逼'

--- app/src/router.js
+++ app/src/router.js
@@ -2,15 +2,21 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Login from '@/pages/login'
 import Home from '@/pages/home'
-
-
+import { Provider } from 'react-redux'
+import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/lib/integration/react'
+import {store} from "./store"
 export default function Router() {
 	return (
-		<BrowserRouter>
-			<Switch>
-				<Route path="/login" component={Login} />
-				<Route path="/" component={Home} />
-			</Switch>
-		</BrowserRouter>
+		<Provider store={store}>
+			<PersistGate loading={null} persistor={persistStore(store)}>
+				<BrowserRouter>
+					<Switch>
+						<Route path="/login" component={Login} />
+						<Route path="/" component={Home} />
+					</Switch>
+				</BrowserRouter>
+			</PersistGate>
+		</Provider>
 	)
 }
