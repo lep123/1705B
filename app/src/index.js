c1062828f1d50f0b767b0fe40bdca4872bd72b43
bubupu <3010674713@qq.com>
2020-01-02 20:02:34 +0800

    000

--- app/src/index.js
+++ app/src/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import Route from '@/router'
-import './index.less';
+import 'antd/dist/antd.css'
+import './index.less'
 
 
 ReactDOM.render(
 	<Route />,
-	document.getElementById('root'));
+	document.getElementById('root'))
