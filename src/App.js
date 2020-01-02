79e48a8a7a26492d663e5c9fb6ef84e668e65ccf
lxc <3121870138@qq.com>
2020-01-02 14:21:44 +0800

    '李晓超'

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      {/* 李晓超 1705B  */}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
