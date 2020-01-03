d611821f79e8ade7d9be92bce85244b2bb2d591d
hyl <1374961656@qq.com>
2020-01-03 10:01:40 +0800

    顶顶顶

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,8 +2,7 @@ import React , {Component} from 'react';
 import Left from './left'
 import Right from './right/index'
 import './style.less'
-/*得得得得得得得得得得*/
-//11111111
+
 export default class Home extends Component {
 
     constructor (props) {
@@ -13,7 +12,6 @@ export default class Home extends Component {
             //没有登陆跳到登录页面
             props.history.push('/login')
         }
-
     }
 
     render() {
