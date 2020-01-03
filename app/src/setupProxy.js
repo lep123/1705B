9766b72312e13a78bfbb30faa512626425a2c05c
bubupu <3010674713@qq.com>
2020-01-03 15:02:50 +0800

    end

--- app/src/setupProxy.js
+++ app/src/setupProxy.js
@@ -3,12 +3,7 @@ const proxy = require('http-proxy-middleware')
 module.exports = function (app) {
     app.use(
         proxy('/api', {
-<<<<<<< HEAD
-            target: 'http://api.baxiaobu.com',
-
-=======
             target: 'http://api.baxiaobu.com/',
->>>>>>> 6c85d8a2c04b79d46b9c8a6d158d2e286159180a
             changeOrigin: true,
             pathRewrite: {
                 '^/api': ''
