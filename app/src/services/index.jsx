79bc8a655d3fcd9f66caca689a528ebfbf2bdb76
kb <2264805401@qq.com>
2020-01-03 15:15:05 +0800

    set

--- app/src/services/index.jsx
+++ app/src/services/index.jsx
@@ -2,10 +2,10 @@ import a from "./api.js"
 import { post, get } from '@/utils/request'
 import qs from "qs"
 export function login(v) {
-    return post(a.log, qs.stringify(v))
+    return post(a.log,v)
 }
 export function ce(v) {
-    return post(a.reg, qs.stringify(v))
+    return post(a.reg,v)
 }
 export function add(v) {
     return post(a.add, v)
